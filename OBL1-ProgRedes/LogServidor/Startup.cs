60ec99a8b64c921637d659012f751b52636512a2
Julieta Percivale <JP245079@fi365.ort.edu.uy>
2021-11-20 21:20:28 -0300

    con startup

--- OBL1-ProgRedes/LogServidor/Startup.cs
+++ OBL1-ProgRedes/LogServidor/Startup.cs
@@ -27,6 +27,21 @@ namespace LogServidor
             services.AddControllers();
             persistenciaLog = PersistenciaLog.ObtenerPersistencia();
 
+            var queueHostname = Configuration.GetSection("Queue:Hostname").Value;
+            var queuePort = Int32.Parse(Configuration.GetSection("Queue:Port").Value);
+            var queueUsername = Configuration.GetSection("Queue:Username").Value;
+            var queuePassword = Configuration.GetSection("Queue:Password").Value;
+            var queueName = Configuration.GetSection("Queue:Name").Value;
+
+            var factory = new ConnectionFactory() { HostName = queueHostname };
+
+            using IConnection conexion = factory.CreateConnection();
+            using IModel canal = conexion.CreateModel();
+
+            canal.QueueDeclare(queueName, false , false, false, null);
+            ColaServicio queueService = new ColaServicio(canal, persistenciaLog, queueName);
+            services.AddSingleton<ColaServicio>(queueService);
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipelinRabbitMQ.Client.Exceptions.BrokerUnreachableException: 'None of the specified endpoints were reachable'e.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
